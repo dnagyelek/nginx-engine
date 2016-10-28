86f6c5f05e92ea95d63c96a0e4a76dcf85168425
David <dnagyelek@gmail.com>
2016-10-28 21:02:32 +0300

    addedd ssl

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,3 +1,5 @@
+# to run after vagrant up
+
 sudo apt-get update
 
 sudo apt-get upgrade
@@ -12,6 +14,11 @@ sudo ee stack install --nginx
 
 # install php5
 sudo ee stack install --php
+sudo ee stack purge --php
+sudo ee stack remove --php
+
+# install php7
+sudo ee stack install --php7
 
 # install HipHop Machine
 sudo ee stack install --hhvm
@@ -26,5 +33,29 @@ sudo ee stack install --wpcli
 sudo ee stack install --redis
 # sudo ee stack install --phpredisadmin
 
+
+
+Postfix
+sudo ee stack purge --postfix
+sudo ee stack remove --postfix
+
 # install Admin Tools
 # sudo ee stack install --admin
+
+# install phpmyadmin
+sudo ee stack install --phpmyadmin
+
+SSL
+
+sudo openssl genrsa -out "/etc/ssl/drigg.box.key" 2048
+sudo openssl req -new -key "/etc/ssl/drigg.box.key" -out "/etc/ssl/drigg.box.csr"
+
+sudo openssl x509 -req -days 2048 \
+    -in "/etc/ssl/drigg.box.csr" \
+    -signkey "/etc/ssl/drigg.box.key" \
+    -out "/etc/ssl/drigg.box.crt"
+
+ssl_certificate /etc/ssl/drigg.box.crt;
+ssl_certificate_key /etc/ssl/drigg.box.key;
+
+ex - > https://serversforhackers.com/video/self-signed-ssl-certificates-for-development
